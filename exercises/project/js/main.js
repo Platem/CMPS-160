47ae243a1cd3e1331fffa44633a624e64dba58c4
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2017-11-13 14:56:57 -0800

    Better animations? I guess..

--- exercises/project/js/main.js
+++ exercises/project/js/main.js
@@ -1,11 +1,14 @@
+let resizeTimer;
+
 $(function() {
 	/* Toggle Panels */
 	$('.nav-icon').on('click', function(e) {
 		let t = $(this).attr('data-toggle');
 		$(this).toggleClass('opened');
 		$('#' + t).toggleClass('collapsed');
-
-		updateCanvas();
+		setTimeout(function() {
+			updateCanvas(true);
+		}, 500);
 	});
 
 	$('.nav-icon').each(function() {
@@ -14,27 +17,48 @@ $(function() {
 		$('#' + t).toggleClass('collapsed');
 	});
 
-	$(window).on('resize', function () {
-		updateCanvas();
+
+	$(window).on('resize', function(e) {
+		clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(function() {
+			updateCanvas(true);
+		}, 250);
 	});
 
-	updateCanvas();
+	setTimeout(function() {
+		updateCanvas(true);
+	}, 500);
 });
 
-function updateCanvas() {
+function updateCanvas(animate) {
 	let webgl = $('#webgl');
 	let canvas = $('#canvas');
 
 	let w = webgl.width();
 	let h = webgl.height();
+
 	let W = canvas.width() - 20;
 	let H = canvas.height() - 20;
 
-	let ratio = W / w;
-
-	if (h * ratio > H)
-		ratio = H / h;
-
-	webgl.width(w * ratio);
-	webgl.height(h * ratio);
+	if (W > H) {
+		if (animate) {
+			webgl.animate({
+				width: H,
+				height: H
+			}, 500);
+		} else {
+			webgl.width(H);
+			webgl.height(H);
+		}
+	} else if (W < H) {
+		if (animate) {
+			webgl.animate({
+				width: W,
+				height: W
+			}, 500);
+		} else {
+			webgl.width(W);
+			webgl.height(W);
+		}
+	}
 }
